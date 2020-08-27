d19d90a27617c63a6e4cb0f08c841601df9f1271
unknown <gonchiperedo77@gmail.com>
2020-08-27 13:29:30 -0400

    primer commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,17 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  monto = 0;
+  civa =0;
+  cit=0;
+  suma = 0;
+  utilidad = 0;
   constructor() {}
 
+  verificarSimilitud() {
+    this.civa = this.monto * 0.13;
+    this.cit = this.monto * 0.03;
+    this.suma = this.cit + this.civa;
+    this.utilidad = this.monto - this.suma;
+  }
 }
